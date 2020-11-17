c738996b1aa27372062dea1373585a72ecfc8df4
Acalltoredemption <acalltoredemption@gmail.com>
2020-11-17 12:30:51 -0800

    some refactoring in the CommentDetail component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="Hello!" avatarImage={faker.image.avatar()} />
+      <CommentDetail author="Alex" timeAgo="Today at 1:44PM" commentText="How are you doing?" avatarImage={faker.image.avatar()} />
+      <CommentDetail author="Jane" timeAgo="Yesterday at 1:15AM" commentText="This is the best ever" avatarImage={faker.image.avatar()} />
+      <CommentDetail author="Bob" timeAgo="3 Days Ago" commentText="Yeah I can feel it" avatarImage={faker.image.avatar()} />
+      <CommentDetail author="Steve" timeAgo="Two Weeks Ago" commentText="Alright!" avatarImage={faker.image.avatar()} />
+      
     </div>
   );
 };
